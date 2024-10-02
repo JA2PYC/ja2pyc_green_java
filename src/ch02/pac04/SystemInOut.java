e775c27d173b95b76618ed7ed1be97e6c6d2309a
gr2nade <gr2nade@gmail.com>
2024-10-02 19:38:28 +0900

    Add Scanner close

--- src/ch02/pac04/SystemInOut.java
+++ src/ch02/pac04/SystemInOut.java
@@ -13,18 +13,22 @@ public class SystemInOut {
 		System.out.println("이름 : " + name + "\n나이 : " + age + "\n전화 : " + tel1 + "-" + tel2 + "-" + tel3);
 		System.out.print("이름 : " + name + "\n나이 : " + age + "\n전화 : " + tel1 + "-" + tel2 + "-" + tel3 + "\n");
 		System.out.printf("이름 : %s\n나이 : %d\n전화 : %s-%s-%s\n", name, age, tel1, tel2, tel3);
-		
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("첫 번째 수 : ");
-		String strNum1 = scanner.nextLine();
-		
-		System.out.print("두 번째 수 : ");
-		String strNum2 = scanner.nextLine();
-		
-		int num1 = Integer.parseInt(strNum1);
-		int num2 = Integer.parseInt(strNum2);
-		int result = num1 + num2;
-		System.out.println("합 : " + result);
+		try {
+			System.out.print("첫 번째 수 : ");
+			String strNum1 = scanner.nextLine();
+
+			System.out.print("두 번째 수 : ");
+			String strNum2 = scanner.nextLine();
+
+			int num1 = Integer.parseInt(strNum1);
+			int num2 = Integer.parseInt(strNum2);
+			int result = num1 + num2;
+			System.out.println("합 : " + result);
+		} finally {
+			scanner.close();
+		}
 	}
 
 }
--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module ja2pyc_green {
-}
