2d1e9c3a39492b0b84e6a397f87948a91f009db1
gr2nade <gr2nade@gmail.com>
2024-10-10 12:16:36 +0900

    Add Enum & Ch06

---
+++ src/ch06/pac01/ClassExample.java
@@ -0,0 +1,15 @@
+package ch06.pac01;
+
+public class ClassExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassStudent s1 = new ClassStudent();
+		System.out.println("s1 : " + s1);
+		
+		ClassStudent s2 = new ClassStudent();
+		System.out.println("s2 : " + s2);
+		
+	}
+
+}
---
+++ src/ch06/pac02/FieldExample.java
@@ -0,0 +1,10 @@
+package ch06.pac02;
+
+public class FieldExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
