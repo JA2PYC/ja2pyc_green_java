fa38b91eb21147aaf6a0d2f05bf7fcfe13e2d49d
gr2nade <gr2nade@gmail.com>
2024-10-12 22:16:52 +0900

    Add Formula Calculator

--- src/ch06/pac04/MethodQuiz.java
+++ src/ch06/pac04/MethodQuiz.java
@@ -5,8 +5,19 @@ public class MethodQuiz {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// Quiz 1
+		// Quiz 3
+		ClassStudent newStudent = new ClassStudent();
+		newStudent.name = "JAVA";
+		newStudent.ban = 3;
+		newStudent.kor = 90;
+		newStudent.eng = 85;
+		newStudent.math =94;
 		
+		int totalScore = newStudent.getTotal();
+		System.out.println("totalScore : " + totalScore);
+		
+		float avgScore = newStudent.getAverage();
+		System.out.println("avgScore : " + avgScore);
 	}
 
 }
